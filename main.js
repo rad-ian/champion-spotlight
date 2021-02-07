ee7198f92941e4b8b11ae77ab9e967078e61ac69
rad-ian <janek.radlinski@gmail.com>
2021-02-07 20:16:50 +0100

    separate main.css and main.js files

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("siema siema o tej porze każdy wypić może")
+let a = 333
+const b = 444
+console.log(a + b)
+a = 222
+console.log(a + b)
+a = 111
+console.log(a + b)
+a = 0
+console.log(a + b)
