492d56c9306dbd3d1a378ecadb67ef83f06e942c
rad-ian <janek.radlinski@gmail.com>
2021-02-09 19:55:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,38 @@ a = 111
 console.log(a + b)
 a = 0
 console.log(a + b)
+for (let i = 0; i < 20; i++) {
+    if (i % 3 === 0) {
+        if (i % 5 === 0) {
+            console.log("foobar");
+        } else {
+            console.log("foo")
+        }
+    } else if (i % 5 === 0) {
+        console.log("bar")
+    } else {
+        console.log(i);
+    }
+}
+for (let i = 0; i < 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("foobar");
+    } else if (i % 3 === 0) {
+        console.log("foo")
+    } else if (i % 5 === 0) {
+        console.log("bar")
+    } else {
+        console.log(i);
+    }
+}
+for (let i = 0; i < 20; i++) {
+    if (i % 15 === 0) {
+        console.log("foobar");
+    } else if (i % 3 === 0) {
+        console.log("foo")
+    } else if (i % 5 === 0) {
+        console.log("bar")
+    } else {
+        console.log(i);
+    }
+}
