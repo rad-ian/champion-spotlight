e70c9290deb162999a10715a3900ff314d7724f4
rad-ian <janek.radlinski@gmail.com>
2021-02-16 20:21:53 +0100

    update main.js

--- main.js
+++ main.js
@@ -53,3 +53,44 @@ for (let i = 0; i < list.length; i++) {
     console.log({ i, listI: list[i], sum });
 }
 console.log({ sum });
+const list3 = [1, 7, 4, 9, 3, 6, 7, 3, 4, 8, 0, 6, 23, 8, 3, 4]
+for (let i = 0; i < list3.length; i++) {
+    switch (i % 6) {
+        case 0:
+            console.log(i, "jest podzielne przez 6");
+            break
+        case 1:
+            console.log(i, "reszta 1");
+            break
+        case 2:
+        case 3:
+            console.log(i, "reszta 2 albo 3");
+            break
+        default:
+            console.log(i, "reszta 4,5");
+    }
+
+}
+for (let i = list3.length - 1; i >= 0; i--) {
+    console.log(i);
+}
+for (let i = list3.length - 1; i >= 0; i = i - 2) {
+    console.log(i);
+}
+for (let i = list3.length - 1; i >= 0; i -= 3) {
+    console.log(i);
+}
+for (let i = 0; i < list3.length; i += 1) {
+    if (i % 6 === 0) {
+        console.log(i, "jest podzielne przez 6");
+    }
+    else if (i % 6 === 1) {
+        console.log(i, "reszta 1");
+    }
+    else if (i % 6 === 2 || i % 6 === 3) {
+        console.log(i, "reszta 2 albo 3");
+    }
+    else {
+        console.log(i, "reszta 4,5");
+    }
+}
