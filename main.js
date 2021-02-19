9185b17cda231a74e1a47fbcc2a25859fae76c1d
rad-ian <janek.radlinski@gmail.com>
2021-02-19 18:20:25 +0100

    minimum/maximum

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//:)
 console.log("siema siema o tej porze każdy wypić może")
 let a = 333
 const b = 444
@@ -8,6 +9,7 @@ a = 111
 console.log(a + b)
 a = 0
 console.log(a + b)
+//sprawdzanie podzielności
 for (let i = 0; i < 20; i++) {
     if (i % 3 === 0) {
         if (i % 5 === 0) {
@@ -43,6 +45,8 @@ for (let i = 0; i < 20; i++) {
         console.log(i);
     }
 }
+
+//zliczanie sumy
 const list = [1, 2, 3, 3, 4, 4, 4]
 let sum = 0
 console.log(list);
@@ -53,6 +57,10 @@ for (let i = 0; i < list.length; i++) {
     console.log({ i, listI: list[i], sum });
 }
 console.log({ sum });
+
+
+
+//switch statement
 const list3 = [1, 7, 4, 9, 3, 6, 7, 3, 4, 8, 0, 6, 23, 8, 3, 4]
 for (let i = 0; i < list3.length; i++) {
     switch (i % 6) {
@@ -69,17 +77,8 @@ for (let i = 0; i < list3.length; i++) {
         default:
             console.log(i, "reszta 4,5");
     }
-
 }
-for (let i = list3.length - 1; i >= 0; i--) {
-    console.log(i);
-}
-for (let i = list3.length - 1; i >= 0; i = i - 2) {
-    console.log(i);
-}
-for (let i = list3.length - 1; i >= 0; i -= 3) {
-    console.log(i);
-}
+// porównanie else/if ze switch
 for (let i = 0; i < list3.length; i += 1) {
     if (i % 6 === 0) {
         console.log(i, "jest podzielne przez 6");
@@ -94,3 +93,43 @@ for (let i = 0; i < list3.length; i += 1) {
         console.log(i, "reszta 4,5");
     }
 }
+
+//różne kroki i kierunki for
+for (let i = list3.length - 1; i >= 0; i--) {
+    console.log(i);
+}
+for (let i = list3.length - 1; i >= 0; i = i - 2) {
+    console.log(i);
+}
+for (let i = list3.length - 1; i >= 0; i -= 3) {
+    console.log(i);
+}
+
+
+//wybiaranie minimum
+const list4 = [10, 17, 9, 8, 6, 7, 6, 4, 8, 5, 6, 4, 4, 0, 1]
+let obecnieNajmniejsza = list4[0]
+for (let i = 1; i < list4.length; i++) {
+    if (obecnieNajmniejsza > list4[i]) {
+        console.log(`zamieniam ${obecnieNajmniejsza} na ${list4[i]} `);
+        obecnieNajmniejsza = list4[i]
+    }
+    else {
+        console.log(`${list4[i]} >= ${obecnieNajmniejsza}`);
+    }
+}
+console.log(`najmniejsza jest ${obecnieNajmniejsza}`);
+
+//wybiaranie maximum
+const list5 = [2, 1, 4, 5, 5, 4, 7, 10, 9, 2, 11, 10, 13, 11, 1]
+let obecnieNajwiększa = -Number.MAX_SAFE_INTEGER
+for (let i = 0; i < list5.length; i++) {
+    if (obecnieNajwiększa < list5[i]) {
+        console.log(`zamieniam ${obecnieNajwiększa} na ${list5[i]} `);
+        obecnieNajwiększa = list5[i]
+    }
+    else {
+        console.log(`${list5[i]} <= ${obecnieNajwiększa}`);
+    }
+}
+console.log(`najwieksza jest ${obecnieNajwiększa}`);
