63997f95e5687902662520b7eb7e1ffb49b9f452
rad-ian <janek.radlinski@gmail.com>
2021-03-05 19:56:38 +0100

    secondary.js

--- secondary.js
+++ secondary.js
@@ -3,3 +3,11 @@ const skillList = document.getElementById('skills-list')
 
 const fajnofajno = document.getElementsByClassName('fajnofajno')
 console.log(fajnofajno);
+
+const miejsceProdukcji = "made in Szewnia Dolna"
+const bodyList = document.getElementsByTagName("body")
+const body = bodyList[0]
+const miejsceProdukcjiH3 = document.createElement("h3")
+miejsceProdukcjiH3.innerText = miejsceProdukcji
+
+body.appendChild(miejsceProdukcjiH3)    
