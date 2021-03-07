f0caa417671920a8319a29c96b8f27b0133eb5e0
rad-ian <janek.radlinski@gmail.com>
2021-03-07 21:04:34 +0100

    buttons!!!

--- secondary.js
+++ secondary.js
@@ -1,6 +1,7 @@
+/*
 const skillList = document.getElementById('skills-list')
-//console.log(skillList.innerHTML);
-
+console.log(skillList.innerHTML);
+*/
 const fajnofajno = document.getElementsByClassName('fajnofajno')
 console.log(fajnofajno);
 
@@ -10,4 +11,45 @@ const body = bodyList[0]
 const miejsceProdukcjiH3 = document.createElement("h3")
 miejsceProdukcjiH3.innerText = miejsceProdukcji
 
-body.appendChild(miejsceProdukcjiH3)    
+body.appendChild(miejsceProdukcjiH3)
+
+// const button2 = document.querySelector(".button")
+// const button3 = document.querySelector("button")
+// const button4 = document.querySelectorAll(".button")
+// const button5 = document.getElementsByClassName('button')
+//przykłady
+
+const button = document.querySelector(".button")
+const skillList = document.querySelector("#skills-list")
+
+
+function pokaListeSkilli(event) {
+    // console.log("poka Liste Skilli");
+    // console.log(event);
+    // console.log(event.target);
+    // console.log(event.target.innerText);
+    if (event.target.innerText === "zobacz wincyj") {
+        skillList.style.display = "block"
+        button.innerText = "zobacz mniyj"
+    }
+    else if (event.target.innerText === "zobacz mniyj") {
+        skillList.style.display = "none"
+        button.innerText = "zobacz wincyj"
+    }
+
+
+    // if (event.offsetX < 50) {
+    //     console.log("po lewo");
+    // }
+    // else if (event.offsetX > 50) {
+    //     console.log("po prawo");
+    // }
+    // else {
+    //     console.log("po środku");
+    // }
+
+
+}
+
+
+button.onclick = pokaListeSkilli
